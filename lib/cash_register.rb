f3caab6a64b192ff448ede4d18e1d9940180ef6f
Elizabeth <mwendemuthusi6@gmail.com>
2023-02-11 15:06:57 +0300

    Completed assignment

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,15 +1,21 @@
 class CashRegister
   attr_accessor :total, :discount, :bought
+  attr_reader :last_bought
 
   def initialize(disc=20)
     @discount= disc
     @total=0
     @bought=[]
   end
+
   def add_item(title, price, quantity=1)
-    self.bought.push(title)
+    for i in 1..quantity
+      @bought<<title
+    end
+    @last_bought=[title, price, quantity]
     @total+=(price*quantity)
   end
+
   def apply_discount
     if @total ==0
       "There is no discount to apply."
@@ -19,22 +25,23 @@ class CashRegister
     "After the discount, the total comes to $#{@total}."
     end
   end
-  def items
-    # puts "hey"
-    # self.bought<<item
-    @bought
 
+  def items
+    @bought
   end
 
+  def void_last_transaction
+    last_purchase= @last_bought[1]*@last_bought[2]
+    @total-=last_purchase
+  end
 end
 c1= CashRegister.new
-# puts c1.discount
-# puts c1.items
-c1.add_item('eggs', 20, 7)
-c1.add_item('cookies', 16, 7)
-c1.add_item('cookies', 16, 7)
-puts c1.items
-# puts c1.total
+
+
+
+
+
+
 
 
 # puts c1.apply_discount
